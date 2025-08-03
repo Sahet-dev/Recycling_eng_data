7e1162c2d22786d3f99bc2e129a16f0f032958b3
Sahy <kakajansahy@gmail.com>
2025-08-03 03:52:26 +0300

    Implement Cloudflare R2 Presigned URLs

---
+++ database/migrations/2025_07_27_023005_add_video_url_to_units_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('units', function (Blueprint $table) {
+            $table->string('video_url')->nullable()->after('unit');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('units', function (Blueprint $table) {
+            $table->dropColumn('video_url');
+        });
+    }
+
+};
